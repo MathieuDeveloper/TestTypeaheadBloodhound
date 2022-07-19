d44568dec2337197fe2ca8415745f0fac5b60a71
MathieuDeveloper <topfermathieu@yahoo.fr>
2022-07-19 23:59:18 +0200

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -14,16 +14,16 @@
 
 
 var bloodMovies = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
   queryTokenizer: Bloodhound.tokenizers.whitespace,
   // `states` is an array of state names defined in "The Basics"
  
 
   remote: {
     url: 'https://api.themoviedb.org/3/search/movie?api_key=06c5d7152a9119d9afbc2813da1ff6ed&query=%QUERY',
-    wildcard: '%QUERY',     
+    wildcard: '%QUERY',
     
-  },
+  }
 
 });
 
@@ -31,7 +31,7 @@ var bloodMovies = new Bloodhound({
 $('#bloodhound .typeahead').typeahead(null, 
   {
   name: 'title',
-  
+  display:'',
   source: bloodMovies
 });
 
