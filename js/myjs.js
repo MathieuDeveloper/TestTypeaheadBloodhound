bfb8835f9e34dd7f01b3b3d935d128d67c2e2443
MathieuDeveloper <topfermathieu@yahoo.fr>
2022-07-20 23:50:27 +0200

    try something else to parse

--- js/myjs.js
+++ js/myjs.js
@@ -1,18 +1,5 @@
-// constructs the suggestion engine
-/* var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];   */
 
-
-
-
+var myJson
 var bloodMovies = new Bloodhound({
   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
   queryTokenizer: Bloodhound.tokenizers.whitespace,
@@ -35,3 +22,5 @@ $('#bloodhound .typeahead').typeahead(null,
   source: bloodMovies
 });
 
+
+function funcClickTest();
