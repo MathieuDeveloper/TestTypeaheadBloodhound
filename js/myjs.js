0a4198cf03d380d903892a65bfcb55fe19e400e7
MathieuDeveloper <topfermathieu@yahoo.fr>
2022-07-24 12:31:56 +0200

    still trying

--- js/myjs.js
+++ js/myjs.js
@@ -1,26 +1,26 @@
 
-var myJson
-var bloodMovies = new Bloodhound({
+var bestPictures = new Bloodhound({
   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
   queryTokenizer: Bloodhound.tokenizers.whitespace,
-  // `states` is an array of state names defined in "The Basics"
- 
-
+  
   remote: {
     url: 'https://api.themoviedb.org/3/search/movie?api_key=06c5d7152a9119d9afbc2813da1ff6ed&query=%QUERY',
     wildcard: '%QUERY',
     
   }
+});
 
+//var parse1 = JSON.stringify(bestPictures);
+//var parse2 = JSON.parse(parse1).title;
+
+$('#remote .typeahead').typeahead(null, {
+  //name: '',
+  //display: '',
+  source: function(bestPictures){ return JSON.parse(JSON.stringify(bestPictures))}
 });
 
 
-$('#bloodhound .typeahead').typeahead(null, 
-  {
-  name: 'title',
-  
-  source: bloodMovies
-});
 
 
+//url: 'https://api.themoviedb.org/3/search/movie?api_key=06c5d7152a9119d9afbc2813da1ff6ed&query=%QUERY.json',
 function funcClickTest(){};
